ed0133b6f361d62543e814d54252981c5202a37a
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-19 13:35:10 +0300

    created a constructor with the quote properties

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,1 +1,7 @@
+export class Quote {
     showDescription: boolean;
+    constructor(public quote:string,public author:string , public posted:string,  public genre:string,public PostDate:  Date ,public upvote:number){
+        this.showDescription = false;
+        
+    }
+} 
