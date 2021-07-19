cd61143bc54c11734224231c1cb0493dd18e3ce8
Becka7 <rabecca.nzau@student.moringaschool.com>
2021-07-19 13:55:11 +0300

    added the increase and decease likes functiions

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,2 +1,36 @@
-  @Input () quote :any;
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 
+@Component({
+  selector: 'app-quote-detail',
+  templateUrl: './quote-detail.component.html',
+  styleUrls: ['./quote-detail.component.css']
+})
+export class QuoteDetailComponent implements OnInit {
+
+  @Input () quote :any;
+  @Output () isComplete = new EventEmitter<boolean>();
+  @Output() toIncreaseLikes = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  quoteComplete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
+  increaseLikes(increase: boolean){
+    this.toIncreaseLikes.emit(increase);
+  
+  }
+
+  dislike: number = 0;
+    decreaseLikes(){
+      return this.dislike++;
+  }
+    
+
+
+
+}
